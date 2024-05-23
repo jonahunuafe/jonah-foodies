f958c17a0cea2a60b69f10645497391dd901be87
Jonah Unuafe <fejisfejiro@gmail.com>
2024-05-23 17:21:53 +0100

    outputting meal data and images with unknown dimensions

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,6 +1,28 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+import MealsGrid from "@/components/meals/meals-grid";
+
+
 export default function MealsPage() {
     return (    
-        <h1>Meals Page</h1>
-
+        <>
+            <header className={classes.header}>
+                <h1>
+                    Delicious Meal Created 
+                    <span className={classes.highlight}> by Jonah</span>
+                </h1>
+                <p>
+                    Choose your favourite recipe and cook it yourself. It is easy and fun
+                </p>
+                <p className={classes.cta}>
+                    <Link href="/meals/share">
+                        Share Your Favourite Recipe
+                    </Link>
+                </p>
+            </header>
+            <main className={classes.main}>
+                <MealsGrid meals={[]} />
+            </main>
+        </>
     )
 }
